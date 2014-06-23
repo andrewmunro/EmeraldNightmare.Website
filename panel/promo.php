e632b2720ced9a91f0b716978477eab1013f310a
Andrew Munro <andrew.munro@dubitlimited.com>
2014-06-23 15:12:30 +0100

    initial commit

---
+++ panel/promo.php
@@ -0,0 +1,10 @@
+<div id="sidebar-marketing" class="sidebar-module">
+<div class="bnet-offer">
+<!--  -->
+<div class="bnet-offer-bg">
+<a href="search.php" target="_blank" id="ad10069924" class="bnet-offer-image">
+<img src="<?php echo $website['root']; ?>wow/static/images/sidebar/Share/armory.png" width="300" height="250" alt=""/>
+</a>
+</div>
+</div>
+</div>
